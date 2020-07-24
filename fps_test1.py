f1ac290c4763dccc8f9b80f83bd218396d55b90b
pokabu55 <okazumaru@gmail.com>
2020-07-24 15:32:41 +0900

    fpsによる描画のテスト

--- fps_test1.py
+++ fps_test1.py
@@ -9,24 +9,24 @@ pygame.init()
 
 # ウィンドウの作成し、 SURFACE に格納する
 SURFACE = pygame.display.set_mode((400,300))
-pygame.display.set_caption("Just Window")
 
 def main():
 
     """ main routine """
+    sysfont = pygame.font.SysFont(None,36)
+    counter = 0
+
     while True:
 
-        # RGBを指定して色を塗る
-        SURFACE.fill((255,255,255))
-
         # イベントキューからイベントを取得
-        for event in pygame.event.get():
+        for event in pygame.event.get(QUIT):
+            pygame.quit()
+            sys.exit()
 
-            # 終了ボタン待ち
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
+        counter += 1
+        SURFACE.fill((0,0,0))
+        count_image = sysfont.render("count is {}".format(counter), True, (255,255,255))
+        SURFACE.blit(count_image, (50,50))
         pygame.display.update()
 
 if __name__ == "__main__":
--- fps_test2.py
+++ fps_test2.py
@@ -6,28 +6,32 @@ from pygame.locals import QUIT
 
 # pygame モジュールの初期化
 pygame.init()
-
-# ウィンドウの作成し、 SURFACE に格納する
 SURFACE = pygame.display.set_mode((400,300))
-pygame.display.set_caption("Just Window")
+# クロックオブジェクト
+FPSCLOCK = pygame.time.Clock()
 
 def main():
 
     """ main routine """
-    while True:
+    sysfont = pygame.font.SysFont(None,36)
+    counter = 0
 
-        # RGBを指定して色を塗る
-        SURFACE.fill((255,255,255))
+    while True:
 
         # イベントキューからイベントを取得
         for event in pygame.event.get():
-
-            # 終了ボタン待ち
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
+        counter += 1
+        SURFACE.fill((0,0,0))
+        count_image = sysfont.render("count is {}".format(counter), True, (255,255,255))
+        SURFACE.blit(count_image, (50,50))
         pygame.display.update()
+
+        # 10 FPS（1秒間に何回実行するかの調整）
+        FPSCLOCK.tick(10)
 
 if __name__ == "__main__":
     main()
